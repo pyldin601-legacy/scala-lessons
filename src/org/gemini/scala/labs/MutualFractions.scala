4992ff70ca89aad3297094594cb085b4bca8b22b
pldin601 <roman@homefs.biz>
2015-06-22 22:07:49 +0300

    MutualFractions

--- src/org/gemini/scala/labs/MutualFractions.scala
+++ src/org/gemini/scala/labs/MutualFractions.scala
@@ -7,7 +7,7 @@ object MutualFractions extends App {
   def calculateFractions(number: Int): Seq[Int] = {
     def isFraction(x: Int) = number % x == 0
     val list = (2 to sqrt(number).toInt + 1).filter(isFraction)
-    list union list reverseMap(number / _)
+    list union list.reverseMap(number / _)
   }
 
   def calculateMutualFractions(value: Int*): Seq[Int] = {
