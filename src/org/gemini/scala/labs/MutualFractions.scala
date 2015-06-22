f69b8e11618ae72952e8a3ad00410d6400d5cd63
pldin601 <roman@homefs.biz>
2015-06-22 22:04:13 +0300

    MutualFractions

--- src/org/gemini/scala/labs/MutualFractions.scala
+++ src/org/gemini/scala/labs/MutualFractions.scala
@@ -4,15 +4,15 @@ import math.sqrt
 
 object MutualFractions extends App {
 
-  def isFraction(of: Int)(that: Int): Boolean = of % that == 0
-
-  def calculateFractions(of: Int): Seq[Int] = {
-    val list = (2 to sqrt(of).toInt + 1).filter(isFraction(of))
-    list union list.map(of / _)
+  def calculateFractions(number: Int): Seq[Int] = {
+    val list = (2 to sqrt(number).toInt + 1).filter(
+      number % _ == 0)
+    list union list.map(number / _)
   }
 
   def calculateMutualFractions(value: Int*): Seq[Int] = {
-    value.map(calculateFractions).reduce(_ intersect _).sorted
+    value.map(calculateFractions).reduce(
+      _ intersect _).sorted
   }
 
   println(calculateMutualFractions(512, 256, 1024))
