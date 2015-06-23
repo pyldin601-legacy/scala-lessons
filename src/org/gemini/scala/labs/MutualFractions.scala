97852d6e3de821d60b621ad61572ae4f33387a15
pldin601 <roman@homefs.biz>
2015-06-23 12:26:06 +0300

    MutualFractions with benchmark

--- src/org/gemini/scala/labs/MutualFractions.scala
+++ src/org/gemini/scala/labs/MutualFractions.scala
@@ -4,6 +4,13 @@ import math.sqrt
 
 object MutualFractions extends App {
 
+  def isPrime(number: Int): Boolean =
+    number != 1 && 2.until(sqrt(number).toInt + 1).forall(number % _ != 0)
+
+  def calculatePrimes(number: Int): Seq[Int] = {
+    2 +: (3 until (number, 2)).filter(isPrime)
+  }
+
   def calculateFractions(number: Int): Seq[Int] = {
     def isFraction(x: Int) = number % x == 0
     val list = (2 to sqrt(number).toInt + 1).filter(isFraction)
@@ -13,15 +20,17 @@ object MutualFractions extends App {
   def calculateMutualFractions(value: Int*): Seq[Int] = {
     value.map(calculateFractions).reduce(_ intersect _)
   }
+  
 
-  var counter = 0
-  val start = System.currentTimeMillis()
 
-  while (System.currentTimeMillis() - start < 5000) {
-    calculateMutualFractions(1024, 2048, 512, 323767, 128)
-    counter += 1
-  }
+//  var counter = 0
+//  val start = System.currentTimeMillis()
+//
+//  while (System.currentTimeMillis() - start < 5000) {
+//    calculateMutualFractions(1024, 2048, 512, 323767, 128)
+//    counter += 1
+//  }
 
-  print(counter)
+
 
 }
