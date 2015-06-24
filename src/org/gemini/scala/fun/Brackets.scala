165517a07c084cad1b2ea81a6d718b631743d496
pldin601 <roman@homefs.biz>
2015-06-24 17:30:44 +0300

    Brackets validator.

--- src/org/gemini/scala/fun/Brackets.scala
+++ src/org/gemini/scala/fun/Brackets.scala
@@ -13,22 +13,23 @@ object Brackets extends App {
    */
   def validateBrackets(str: String): Boolean = {
     val brackets = Array('(', ')', '[', ']', '{', '}')
-    def _val(chars: Array[Char], current: Option[Int]): Boolean = {
+    def _val(chars: Array[Char], current: List[Int] = Nil): Boolean = {
       chars.isEmpty match {
         case true => current.isEmpty
         case _ =>
           brackets.indexOf(chars.head) match {
             case -1 => _val(chars.tail, current)
             case index =>
-              if ((index & 1) == 0) _val(chars.tail, Some(index))
-              else if (current.isDefined && index - current.get == 1) true
+              if ((index & 1) == 0) _val(chars.tail, current :+ index)
+              else if (current.nonEmpty && index - current.last == 1)
+                _val(chars.tail, current.takeRight(0))
               else false
           }
 
       }
 
     }
-    _val(str.toCharArray, None)
+    _val(str.toCharArray)
   }
 
   val testLines = Array(
