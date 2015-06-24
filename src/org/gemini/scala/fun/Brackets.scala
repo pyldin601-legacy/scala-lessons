b9a0ba20ce66bc6a5c869c48e2b782f72f929552
pldin601 <roman@homefs.biz>
2015-06-24 14:30:44 +0300

    Brackets validator.

--- src/org/gemini/scala/fun/Brackets.scala
+++ src/org/gemini/scala/fun/Brackets.scala
@@ -1,38 +1,44 @@
 package org.gemini.scala.fun
 
-import scala.collection.mutable
-
 /**
  * Created by Roman on 24.06.2015.
  */
 object Brackets extends App {
 
   /**
-   * Validates the correctness of nested brackets
-   * @param str String to validate
-   * @return
+   * Validates the correctness of nested brackets and quotes
+   * @param str String with text to validate
+   * @return Boolean
    */
-  def validate(str: String): Boolean = {
-    val brackets = Array('(', ')', '[', ']', '{', '}')
+  def validateBrackets(str: String): Boolean = {
+    val brackets = Array('(', ')', '[', ']', '{', '}', ''', '"')
     val filtered = str.filter(brackets contains _).toCharArray
+    val quoteOffset = 6
     def _val(chars: Array[Char], current: Option[Int] = None): Boolean = {
       if (chars.isEmpty) return current.isEmpty
 
       val index = brackets.indexOf(chars.head)
 
-      if ((index & 1) == 0) _val(chars.tail, Some(index))
+      if (current.isDefined && index >= quoteOffset && index == current.get) true
+      else if ((index & 1) == 0 || index >= quoteOffset) _val(chars.tail, Some(index))
       else if (current.isDefined && index - current.get == 1) true
       else false
     }
     _val(filtered)
   }
 
-  println(validate("[Hello]"))
-  println(validate("[Hello{}]()"))
-  println(validate("Test[(])"))
-  println(validate("Test Test"))
-  println(validate("{{{{}}}}"))
-  println(validate("{hello"))
-  println(validate("hello}"))
+  val testLines = Array(
+    "[Hello]",
+    "[Hello{}]()",
+    "Test 'this' [(])",
+    "Test 'this may { fail' Test",
+    "Test 'this may { fail' Test",
+    "{{{{}}}}",
+    "{hello", "hello}",
+    "Text with \"correct [hello]\" quotes",
+    "Text with \"incorrect {\" quotes}"
+  )
+
+  testLines.map((t) => t + " -> " + validateBrackets(t)).foreach(println)
 
 }
