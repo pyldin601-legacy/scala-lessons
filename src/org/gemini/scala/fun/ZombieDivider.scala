572a1717c4b1678ed49492b0faa75d55c152ba26
pldin601 <roman@homefs.biz>
2015-06-25 12:22:15 +0300

    Divider

--- src/org/gemini/scala/fun/ZombieDivider.scala
+++ src/org/gemini/scala/fun/ZombieDivider.scala
@@ -4,17 +4,18 @@ package org.gemini.scala.fun
  * Created by Roman on 25.06.2015
  */
 object ZombieDivider extends App {
-  case class Zombie() {
+  class Zombie() {
     def /(q: Int): List[Zombie] = {
-      List.fill(q)(this)
+      List.fill(q)(new Zombie)
     }
+    override def toString: String = "Zombie!"
   }
 
-  val zombie = Zombie()
+  val zombie = new Zombie()
 
   println(zombie)
 
-  println(zombie / 10)
+  println((zombie / 10).toSet.subsets().map(_.toList).toList)
 
 }
 
