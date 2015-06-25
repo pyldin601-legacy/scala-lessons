bdb26eefbaf3e429695d6e70b580bed8a4fb74fe
pldin601 <roman@homefs.biz>
2015-06-25 11:45:31 +0300

    Brackets validator.

---
+++ src/org/gemini/scala/fun/DoneIsBetter.scala
@@ -0,0 +1,20 @@
+package org.gemini.scala.fun
+
+/**
+ * Created by Roman on 25.06.2015.
+ */
+
+
+object DoneIsBetter extends App {
+
+  object Done {
+    def is(a: better.type): Done.type = { Done.this }
+    def than(b: perfect.type): String = "You wright, dude! ;)"
+  }
+
+  object better
+  object perfect
+
+  println(Done is better than perfect)
+
+}
