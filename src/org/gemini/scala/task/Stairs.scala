47fa7ef3538eda96ea97f0d7416fa34b94b3be29
pldin601 <roman@homefs.biz>
2015-07-10 16:59:52 +0300

    "Лесенка"

---
+++ src/org/gemini/scala/task/Stairs.scala
@@ -0,0 +1,28 @@
+package org.gemini.scala.task
+
+/*
+ * Ћесенкой называетс€ набор кубиков, в котором каждый более верхний
+ * слой содержит кубиков меньше, чем предыдущий. “ребуетс€ написать
+ * программу, вычисл€ющую число лесенок, которое можно построить
+ * из N кубиков.
+ */
+
+/**
+ * Created by Roman on 10.07.2015
+ */
+object Stairs extends App {
+
+  def compareAll[A](list: List[A], p: (A, A) => Boolean): Boolean = {
+    if (list.isEmpty || list.tail.isEmpty) true
+    else if (p(list.head, list.tail.head)) compareAll(list.tail, p)
+    else false
+  }
+
+  def partNumber(num: Int): List[List[Int]] =
+    List(num) :: (1 until num).flatMap(n => partNumber(num - n).map(n :: _)).toList
+
+  def calc(arr: Int) = partNumber(arr).count(a => compareAll[Int](a, _ < _))
+
+  println(calc(10))
+
+}
