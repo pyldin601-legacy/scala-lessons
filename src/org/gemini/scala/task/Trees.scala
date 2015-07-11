1f08f8f08d8dc07df31761532a69b8f9f0660258
pldin601 <roman@homefs.biz>
2015-07-11 17:48:12 +0300

    Functional Pearl

--- src/org/gemini/scala/task/Trees.scala
+++ src/org/gemini/scala/task/Trees.scala
@@ -8,8 +8,9 @@ object Trees extends App {
   def trees(total: Int, need: Int) =
     if (need == 1) total
     else (0 to total - need).map(x => need + ((need - 1) * x)).
-      filter(_ <= total).map(total - _ + 1).sum
+      filter(_ <= total).map(total - _ + 1).
+      sum
 
-  println(trees(100, 20))
+  println(trees(5, 3))
 
 }
