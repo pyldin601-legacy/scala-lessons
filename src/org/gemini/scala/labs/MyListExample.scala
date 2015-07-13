ec27c3e598df7e5f613567103964043b45194888
pldin601 <roman@homefs.biz>
2015-07-13 09:44:16 +0300

    "MyListExample"

--- src/org/gemini/scala/labs/MyListExample.scala
+++ src/org/gemini/scala/labs/MyListExample.scala
@@ -6,27 +6,27 @@ package org.gemini.scala.labs
 object MyListExample extends App {
 
   sealed abstract class MyList[+E] {
+
     def isEmpty: Boolean
     def head: E
     def tail: MyList[E]
 
     def ::[B >: E](item: B): MyList[B] = {
-      new MyListItem[B](item, this)
+      org.gemini.scala.labs.MyListExample.::[B](item, this)
     }
+
   }
 
-  object MyNil extends MyList[Nothing] {
+  case object MyNil extends MyList[Nothing] {
     def isEmpty: Boolean = true
     def head: Nothing = throw new NoSuchElementException
     def tail: MyList[Nothing] = throw new NoSuchElementException
   }
 
-  case class MyListItem[E](private[scala] val item: E, private[scala] val tl: MyList[E]) extends MyList[E] {
+  case class ::[E](override val head: E, private[scala] val tl: MyList[E]) extends MyList[E] {
     def isEmpty: Boolean = false
-    def head: E = item
     def tail: MyList[E] = tl
   }
-
 
   val list = 1 :: 2 :: 3 :: MyNil
   
