00a747dd64d93857700248d03c0fdd4efa19db42
pldin601 <roman@homefs.biz>
2015-07-13 10:45:28 +0300

    "MyListExample"

---
+++ src/org/gemini/scala/labs/Euclid.scala
@@ -0,0 +1,15 @@
+package org.gemini.scala.labs
+
+/**
+ * Created by Roman on 13.07.2015
+ */
+object Euclid extends App {
+
+  def euclid(a: Int, b: Int): Int = {
+    if (b == 0) a
+    else euclid(b, a % b)
+  }
+
+  println(euclid(1000, 340))
+
+}
