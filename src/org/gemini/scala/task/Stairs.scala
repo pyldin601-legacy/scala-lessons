fe1a84e303843c1dfeddce48dce0923eb6c226be
pldin601 <roman@homefs.biz>
2015-07-14 12:23:59 +0300

    "Trees"

--- src/org/gemini/scala/task/Stairs.scala
+++ src/org/gemini/scala/task/Stairs.scala
@@ -16,16 +16,16 @@ object Stairs extends App {
     (list zip list.tail).forall(t => p(t._1, t._2))
   }
 
-  def partNumber(num: Int): List[List[Int]] =
+  def numberPartitions(num: Int): List[List[Int]] =
     List(num) :: (1 until num).
-      flatMap(n => partNumber(num - n).map(n :: _)).
+      flatMap(n => numberPartitions(num - n).map(n :: _)).
       toList.filter(a => matchAllZippedPairs[Int](a, _ < _))
 
   def loop(): Unit = {
     val input = try scala.io.StdIn.readLine("Enter cubes count: ").toInt catch {
       case e: NumberFormatException => println("Bye!"); return
     }
-    println("Stairs count: " + partNumber(input).length)
+    println("Stairs count: " + numberPartitions(input).length)
     loop()
   }
 
